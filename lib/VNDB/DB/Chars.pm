MewX/yorhel-vndb

commit 8802eac4f345f5a1d4fe8cf8e0b5ada3d9bc857e

DB::Chars: Ignore spaces when matching original names
https://vndb.org/t950.432

lib/VNDB/DB/Chars.pm
@@ -42,7 +42,7 @@ sub dbCharGet {
     defined $o{weight_min} ? ( 'c.weight >= ?' => $o{weight_min} ) : (),
     defined $o{weight_max} ? ( 'c.weight <= ? AND c.weight > 0' => $o{weight_max} ) : (),
     $o{search} ? (
-      '(c.name ILIKE ? OR c.original ILIKE ? OR c.alias ILIKE ?)', [ map '%'.$o{search}.'%', 1..3 ] ) : (),
+      "(c.name ILIKE ? OR translate(c.original,' ','') ILIKE translate(?,' ','')) OR c.alias ILIKE ?", [ map '%'.$o{search}.'%', 1..3 ] ) : (),
     $o{char} ? (
       'LOWER(SUBSTR(c.name, 1, 1)) = ?' => $o{char} ) : (),
     defined $o{char} && !$o{char} ? (

commit 2586f9c78693705807d6c8122ec36eae5de4a33d

DB::Chars: Fix SQL operator precedence bug in search filter
This would cause other filters to sometimes be ignored, depending on the
order in which the %where hash was being serialized.
https://vndb.org/t11605.3

Broken in 8802eac4f.

lib/VNDB/DB/Chars.pm
@@ -42,7 +42,7 @@ sub dbCharGet {
     defined $o{weight_min} ? ( 'c.weight >= ?' => $o{weight_min} ) : (),
     defined $o{weight_max} ? ( 'c.weight <= ? AND c.weight > 0' => $o{weight_max} ) : (),
     $o{search} ? (
-      "(c.name ILIKE ? OR translate(c.original,' ','') ILIKE translate(?,' ','')) OR c.alias ILIKE ?", [ map '%'.$o{search}.'%', 1..3 ] ) : (),
+      "(c.name ILIKE ? OR translate(c.original,' ','') ILIKE translate(?,' ','') OR c.alias ILIKE ?)", [ map '%'.$o{search}.'%', 1..3 ] ) : (),
     $o{char} ? (
       'LOWER(SUBSTR(c.name, 1, 1)) = ?' => $o{char} ) : (),
     defined $o{char} && !$o{char} ? (