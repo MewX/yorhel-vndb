409ea71adee83d05b53f14d2be10abc153905ca9
Yorhel <git@yorhel.nl>
2019-09-03 10:12:59 +0200

    Now with the update script

---
+++ util/updates/update_20190903.sql
@@ -0,0 +1,7 @@
+ALTER TABLE wikidata ADD COLUMN crunchyroll        text[];
+ALTER TABLE wikidata ADD COLUMN igdb_game          text[];
+ALTER TABLE wikidata ADD COLUMN giantbomb          text[];
+ALTER TABLE wikidata ADD COLUMN pcgamingwiki       text[];
+ALTER TABLE wikidata ADD COLUMN steam              integer[];
+ALTER TABLE wikidata ADD COLUMN gog                text[];
+ALTER TABLE wikidata ADD COLUMN pixiv_user         integer[];
