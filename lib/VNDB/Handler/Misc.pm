7e33f7fdbdfd28ff4761262525e9bca0b54e3bc4
Yorhel <git@yorhel.nl>
2019-09-13 12:13:50 +0200

    Fix old usage of $VNDB:ROOT

--- lib/VNDB/Handler/Misc.pm
+++ lib/VNDB/Handler/Misc.pm
@@ -337,8 +337,7 @@ sub opensearch {
    tag 'ShortName', 'VNDB';
    tag 'LongName', 'VNDB.org visual novel search';
    tag 'Description', 'Search visual vovels on VNDB.org';
-   tag 'Image', width => 16, height => 16, type => 'image/x-icon', "$h/favicon.ico"
-     if -s "$VNDB::ROOT/www/favicon.ico";
+   tag 'Image', width => 16, height => 16, type => 'image/x-icon', "$h/favicon.ico";
    tag 'Url', type => 'text/html', method => 'get', template => "$h/v/all?q={searchTerms}", undef;
    tag 'Url', type => 'application/opensearchdescription+xml', rel => 'self', template => "$h/opensearch.xml", undef;
    tag 'Query', role => 'example', searchTerms => 'Tsukihime', undef;
