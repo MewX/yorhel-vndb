aa27b54fa3245a4f4763502db0007a3eeb47ec47
Yorhel <git@yorhel.nl>
2019-09-23 14:54:12 +0200

    Add 4 new languages
    https://vndb.org/t12897.6

---
+++ util/updates/update_20190923.sql
@@ -0,0 +1,4 @@
+ALTER TYPE language ADD VALUE 'mk' AFTER 'ko';
+ALTER TYPE language ADD VALUE 'lt' AFTER 'mk';
+ALTER TYPE language ADD VALUE 'lv' AFTER 'lt';
+ALTER TYPE language ADD VALUE 'sl' AFTER 'sk';
