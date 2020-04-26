MewX/yorhel-vndb

commit ba552a7efe903181eb7fcb2f64854c64aadb7b25

Ignore existing tag votes by users without perm_tag
Same idea as 00fbcdf96432efad85abdbc7392d493409a4c253. Not sure why it
hadn't occurred to me before, seems like such an obvious thing to do.

lib/VNWeb/VN/Tagmod.pm
@@ -73,11 +73,12 @@ TUWF::get qr{/$RE{vid}/tagmod}, sub {
 
     my $tags = tuwf->dbAlli('
         SELECT t.id, t.name, t.cat, count(*) as count, t.applicable
-             , avg(CASE WHEN tv.ignore THEN NULL ELSE tv.vote END) as rating
-             , coalesce(avg(CASE WHEN tv.ignore THEN NULL ELSE tv.spoiler END), t.defaultspoil) as spoiler
+             , avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.vote END) as rating
+             , coalesce(avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.spoiler END), t.defaultspoil) as spoiler
              , bool_or(tv.ignore) as overruled
           FROM tags t
           JOIN tags_vn tv ON tv.tag = t.id
+          LEFT JOIN users u ON u.id = tv.uid
          WHERE tv.vid =', \$v->{id}, '
          GROUP BY t.id, t.name, t.cat
          ORDER BY t.name'

elm/User/Edit.elm
@@ -190,7 +190,7 @@ view model =
         , perm opts.perm_dbmod    <| label [] [ inputCheck "" m.perm_edit     (Admin << PermEdit),     text " edit*", br_ 1 ]
         , perm opts.perm_imgmod   <| label [] [ inputCheck "" m.perm_imgvote  (Admin << PermImgvote),  text " imgvote* (existing votes will stop counting when unset)", br_ 1 ]
         , perm False              <| label [] [ inputCheck "" m.perm_imgmod   (Admin << PermImgmod),   text " imgmod", br_ 1 ]
-        , perm opts.perm_tagmod   <| label [] [ inputCheck "" m.perm_tag      (Admin << PermTag),      text " tag*", br_ 1 ]
+        , perm opts.perm_tagmod   <| label [] [ inputCheck "" m.perm_tag      (Admin << PermTag),      text " tag* (existing tag votes will stop counting when unset)", br_ 1 ]
         , perm False              <| label [] [ inputCheck "" m.perm_dbmod    (Admin << PermDbmod),    text " dbmod", br_ 1 ]
         , perm False              <| label [] [ inputCheck "" m.perm_tagmod   (Admin << PermTagmod),   text " tagmod", br_ 1 ]
         , perm False              <| label [] [ inputCheck "" m.perm_usermod  (Admin << PermUsermod),  text " usermod", br_ 1 ]

lib/VNWeb/Tags/Links.pm
@@ -67,7 +67,7 @@ TUWF::get qr{/g/links}, sub {
 
     my $count = $filt && tuwf->dbVali('SELECT COUNT(*) FROM tags_vn tv WHERE', $where);
     my($lst, $np) = tuwf->dbPagei({ page => $opt->{p}, results => 50 }, '
-        SELECT tv.vid, tv.uid, tv.tag, tv.vote, tv.spoiler,', sql_totime('tv.date'), 'as date, tv.ignore, tv.notes, v.title,', sql_user(), ', t.name
+        SELECT tv.vid, tv.uid, tv.tag, tv.vote, tv.spoiler,', sql_totime('tv.date'), 'as date, tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) AS ignore, tv.notes, v.title,', sql_user(), ', t.name
           FROM tags_vn tv
           JOIN vn v ON v.id = tv.vid
           LEFT JOIN users u ON u.id = tv.uid

lib/VNWeb/VN/Page.pm
@@ -256,12 +256,13 @@ sub infobox_anime_ {
 
 sub infobox_tags_ {
     my($v) = @_;
-    my $rating = 'avg(CASE WHEN tv.ignore THEN NULL ELSE tv.vote END)';
+    my $rating = 'avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.vote END)';
     my $tags = tuwf->dbAlli("
         SELECT t.id, t.name, t.cat, count(*) as cnt, $rating as rating
-             , coalesce(avg(CASE WHEN tv.ignore THEN NULL ELSE tv.spoiler END), t.defaultspoil) as spoiler
+             , coalesce(avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.spoiler END), t.defaultspoil) as spoiler
           FROM tags t
           JOIN tags_vn tv ON tv.tag = t.id
+          LEFT JOIN users u ON u.id = tv.uid
          WHERE t.state = 1+1 AND tv.vid =", \$v->{id}, "
          GROUP BY t.id, t.name, t.cat
         HAVING $rating > 0

sql/func.sql
@@ -221,7 +221,9 @@ BEGIN
         SELECT tv.tag, tv.vid, AVG(tv.vote)::real, CASE WHEN COUNT(tv.spoiler) = 0 THEN MIN(t.defaultspoil) ELSE AVG(tv.spoiler)::real END
           FROM tags_vn tv
           JOIN tags t ON t.id = tv.tag
+          LEFT JOIN users u ON u.id = tv.uid
          WHERE NOT tv.ignore AND t.state = 2
+           AND (u.id IS NULL OR u.perm_tag)
            AND vid NOT IN(SELECT id FROM vn WHERE hidden)
            AND (uvid IS NULL OR vid = uvid)
          GROUP BY tv.tag, tv.vid

sql/schema.sql
@@ -810,7 +810,7 @@ CREATE TABLE users (
   perm_dbmod      boolean NOT NULL DEFAULT false,
   perm_edit       boolean NOT NULL DEFAULT true,
   perm_imgvote    boolean NOT NULL DEFAULT true, -- [pub] (public because this is used in calculating image flagging scores)
-  perm_tag        boolean NOT NULL DEFAULT true,
+  perm_tag        boolean NOT NULL DEFAULT true, -- [pub] (public because this is used in calculating VN tag scores)
   perm_tagmod     boolean NOT NULL DEFAULT false,
   perm_usermod    boolean NOT NULL DEFAULT false,
   perm_imgmod     boolean NOT NULL DEFAULT false

commit db7c295800d30208b7e48ebae593cabe867c3e7a

Fix loading of /v+/tagmod with tags with only ignored voted
Broken in ba552a7efe903181eb7fcb2f64854c64aadb7b25

https://vndb.org/t13884

lib/VNWeb/VN/Tagmod.pm
@@ -73,7 +73,7 @@ TUWF::get qr{/$RE{vid}/tagmod}, sub {
 
     my $tags = tuwf->dbAlli('
         SELECT t.id, t.name, t.cat, count(*) as count, t.applicable
-             , avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.vote END) as rating
+             , coalesce(avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.vote END), 0) as rating
              , coalesce(avg(CASE WHEN tv.ignore OR (u.id IS NOT NULL AND NOT u.perm_tag) THEN NULL ELSE tv.spoiler END), t.defaultspoil) as spoiler
              , bool_or(tv.ignore) as overruled
           FROM tags t