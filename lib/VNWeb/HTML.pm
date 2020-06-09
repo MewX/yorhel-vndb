MewX/yorhel-vndb

commit 1e83c2ad0ee99b8ace40af70aedf17a3ec41b205

Custom CSS: No need to escape double quotes
&quot; breaks CSS selectors like a[href="/"].

lib/VNWeb/HTML.pm
@@ -120,18 +120,20 @@ sub elm_ {
 sub _sanitize_css {
     # This function is attempting to do the impossible: Sanitize user provided
     # CSS against various attacks.  I'm not expecting this to be bullet-proof.
-    # This function doesn't bother with HTML injection as the output will go
-    # through xml_escape(). Fortunately, we also have CSP in place to mitigate
-    # some problems if they arise, but I'd rather not rely on it.
-    # I'd *love* to disable support for external url()'s, but unfortunately
-    # many people use that to load images. I'm afraid the only way to work
-    # around that is to fetch and cache those URLs on the server.
+    # Fortunately, we also have CSP in place to mitigate some problems if they
+    # arise, but I'd rather not rely on it.  I'd *love* to disable support for
+    # external url()'s, but unfortunately many people use that to load images.
+    # I'm afraid the only way to work around that is to fetch and cache those
+    # URLs on the server.
     local $_ = $_[0];
     s/\\//g; # Get rid of backslashes, could be used to bypass the other regexes.
     s/@(import|charset|font-face)[^\n\;]*.//ig;
     s/javascript\s*://ig; # Not sure 'javascript:' URLs do anything, but just in case.
     s/expression\s*\(//ig; # An old IE thing I guess.
     s/binding\s*://ig; # Definitely don't want bindings.
+    s/&/&amp;/g;
+    s/</&lt;/g;
+    s/>/&gt;/g;
     $_;
 }
 
@@ -153,7 +155,7 @@ sub _head_ {
     link_ rel => 'shortcut icon', href => '/favicon.ico', type => 'image/x-icon';
     link_ rel => 'stylesheet', href => config->{url_static}.'/s/'.$skin.'/style.css?'.config->{version}, type => 'text/css', media => 'all';
     link_ rel => 'search', type => 'application/opensearchdescription+xml', title => 'VNDB VN Search', href => tuwf->reqBaseURI().'/opensearch.xml';
-    style_ type => 'text/css', _sanitize_css($customcss) if $customcss;
+    style_ type => 'text/css', sub { lit_ _sanitize_css $customcss } if $customcss;
     if($o->{feeds}) {
         link_ rel => 'alternate', type => 'application/atom+xml', href => "/feeds/announcements.atom", title => 'Site Announcements';
         link_ rel => 'alternate', type => 'application/atom+xml', href => "/feeds/changes.atom",       title => 'Recent Changes';

commit 3f42102ab7a0a2031e90f496496fde31389f7c53

HTML: Fix ">" character in custom css
Broken in 1e83c2ad0ee99b8ace40af70aedf17a3ec41b205. Turns out
TUWF::XML's not very conservative escaping was a feature in this case.

lib/VNWeb/HTML.pm
@@ -133,7 +133,6 @@ sub _sanitize_css {
     s/binding\s*://ig; # Definitely don't want bindings.
     s/&/&amp;/g;
     s/</&lt;/g;
-    s/>/&gt;/g;
     $_;
 }
 