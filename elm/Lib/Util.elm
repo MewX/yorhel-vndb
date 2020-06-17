7bac5611f8b80cfab14b521b03031acd8c059be4
Yorhel <git@yorhel.nl>
2020-06-17 10:13:41 +0200

    Elm: Recognize Hangul as a non-latin language

--- elm/Lib/Util.elm
+++ elm/Lib/Util.elm
@@ -74,7 +74,7 @@ jap_ = Maybe.withDefault Regex.never (Regex.fromString "[\\u3000-\\u9fff\\uff00-
 
 -- Not even close to comprehensive, just excludes a few scripts commonly found on VNDB.
 nonlatin_ : Regex.Regex
-nonlatin_ = Maybe.withDefault Regex.never (Regex.fromString "[\\u3000-\\u9fff\\uff00-\\uff9f\\u0400-\\u04ff]")
+nonlatin_ = Maybe.withDefault Regex.never (Regex.fromString "[\\u3000-\\u9fff\\uff00-\\uff9f\\u0400-\\u04ff\\u1100-\\u11ff\\uac00-\\ud7af]")
 
 containsJapanese : String -> Bool
 containsJapanese = Regex.contains jap_
