e37e6be169d0fd3ce1d49b9466a54cad59083efd
Yorhel <git@yorhel.nl>
2020-07-26 07:40:01 +0200

    Reports: Allow bbcode in reports & mod comments

--- lib/VNWeb/Misc/Reports.pm
+++ lib/VNWeb/Misc/Reports.pm
@@ -106,7 +106,7 @@ sub report_ {
         lit_ $r->{title};
         br_;
         txt_ $r->{reason};
-        div_ class => 'quote', sub { lit_ TUWF::XML::html_escape $r->{message} } if $r->{message};
+        div_ class => 'quote', sub { lit_ bb2html $r->{message} } if $r->{message};
     };
     td_ style => 'width: 300px', sub {
         form_ method => 'post', action => '/report/edit', sub {
@@ -121,7 +121,7 @@ sub report_ {
         };
     };
     td_ sub {
-        lit_ TUWF::XML::html_escape $r->{log};
+        lit_ bb2html $r->{log};
     };
 }
 
