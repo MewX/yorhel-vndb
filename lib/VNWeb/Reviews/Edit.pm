MewX/yorhel-vndb

commit 89f1916021d64ddb0f8f3fb3fda9843515c74dee

Reviews: Also fix editing permission check

lib/VNWeb/Reviews/Edit.pm
@@ -36,7 +36,7 @@ TUWF::get qr{/$RE{vid}/addreview}, sub {
 
 TUWF::get qr{/$RE{wid}/edit}, sub {
     my $e = tuwf->dbRowi(
-        'SELECT r.id, r.uid, r.vid, r.rid, r.summary, r.text, r.spoiler, v.title AS vntitle
+        'SELECT r.id, r.uid AS user_id, r.vid, r.rid, r.summary, r.text, r.spoiler, v.title AS vntitle
           FROM reviews r JOIN vn v ON v.id = r.vid WHERE r.id =', \tuwf->capture('id')
     );
     return tuwf->resNotFound if !$e->{id};
@@ -54,7 +54,7 @@ elm_api ReviewsEdit => $FORM_OUT, $FORM_IN, sub {
     my($data) = @_;
     my $id = delete $data->{id};
 
-    my $review = $id ? tuwf->dbRowi('SELECT id, uid FROM reviews WHERE id =', \$id) : {};
+    my $review = $id ? tuwf->dbRowi('SELECT id, uid AS user_id FROM reviews WHERE id =', \$id) : {};
     return elm_Unauth if !can_edit w => $review;
 
     validate_dbid 'SELECT id FROM vn WHERE id IN', $data->{vid};

lib/VNWeb/Validation.pm
@@ -207,7 +207,7 @@ sub can_edit {
     if($type eq 'w') {
         return 1 if auth->permBoardmod;
         return auth->permReview if !$entry->{id};
-        return auth && auth->uid == $entry->{uid};
+        return auth && auth->uid == $entry->{user_id};
     }
 
     die "Can't do authorization test when entry_hidden/entry_locked fields aren't present"

commit 4b33a35eba600699d7b7e66acc9792ac1fc7e3f9

reviews: Fix permission check when deleting review
Broken in 89f1916021d64ddb0f8f3fb3fda9843515c74dee

lib/VNWeb/Reviews/Edit.pm
@@ -77,7 +77,7 @@ elm_api ReviewsEdit => $FORM_OUT, $FORM_IN, sub {
 
 elm_api ReviewsDelete => undef, { id => { vndbid => 'w' } }, sub {
     my($data) = @_;
-    my $review = tuwf->dbRowi('SELECT id, uid FROM reviews WHERE id =', \$data->{id});
+    my $review = tuwf->dbRowi('SELECT id, uid AS user_id FROM reviews WHERE id =', \$data->{id});
     return elm_Unauth if !can_edit w => $review;
     auth->audit($review->{uid}, 'review delete', "deleted $review->{id}");
     tuwf->dbExeci('DELETE FROM reviews WHERE id =', \$data->{id});